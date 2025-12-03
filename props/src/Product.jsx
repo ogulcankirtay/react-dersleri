194b4eb49667b9c463f514b971d062d008b92d3d
Ogulcan <ogulcankirtay8@gmail.com>
2025-12-03 22:00:18 +0300

    props worked

--- props/src/Product.jsx
+++ props/src/Product.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Product({ name, price }) {
     return (
-        <>
+        <div>
             <div>Product </div>
             <div>Product Name {name} </div>
 
             <div>Product Price {price}</div>
-        </>
+        </div>
     );
 }
 
